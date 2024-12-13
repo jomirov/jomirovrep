f4b77f66dc480aa388a424e68875c644cdf4a856
jomirov <jandosomirov@gmail.com>
2024-12-13 08:34:03 +0500

    myapp

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
